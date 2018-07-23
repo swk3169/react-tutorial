2be93df5bd0681d8d2e490a250df8b56bb3753be
swk3169 <swk3169@gmail.com>
2018-07-23 23:32:21 +0900

    update-ver0.00

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import MyName from './MyName';
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <MyName name="리액트" />
+    );
+  }
+}
+
+export default App;
